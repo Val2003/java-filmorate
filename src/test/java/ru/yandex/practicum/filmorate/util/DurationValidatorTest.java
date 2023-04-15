49a1d555cae8c31c00d36d46cb8df59b44fffd99
val2003 <val2003@yandex.ru>
2023-04-16 02:38:48 +0400

    reformat code 7

--- src/test/java/ru/yandex/practicum/filmorate/util/DurationValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/util/DurationValidatorTest.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.util;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.Duration;
+
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- src/test/java/ru/yandex/practicum/filmorate/util/StringValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/util/StringValidatorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.Duration;
+
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
