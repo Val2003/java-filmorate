d29a7ffdb86143cfda7f5cdc5a7457d4d5d3e5d0
Val2003 <55923298+Val2003@users.noreply.github.com>
2023-04-15 23:45:30 +0400

    Update LocalDateValidator.java

--- src/main/java/ru/yandex/practicum/filmorate/util/LocalDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/LocalDateValidator.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.util;
 import java.time.LocalDate;
 
 public final class LocalDateValidator {
-private LocalDateValidator(){}
+private LocalDateValidator() {}
     public static boolean isDateTooOld(LocalDate dateToCheck, LocalDate theOldestPossibleDate) {
 
         return dateToCheck.isBefore(theOldestPossibleDate);
