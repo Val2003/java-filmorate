6a1fc858d4a297a5786b55e318d7c7c8733d2b08
Val2003 <55923298+Val2003@users.noreply.github.com>
2023-04-15 23:48:53 +0400

    Update DurationValidator.java

--- src/main/java/ru/yandex/practicum/filmorate/util/DurationValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/DurationValidator.java
@@ -3,7 +3,8 @@ package ru.yandex.practicum.filmorate.util;
 import java.time.Duration;
 
 public final class DurationValidator {
-private DurationValidator() {}
+private DurationValidator() {
+}
     public static boolean isDurationNegativeOrZero(Duration duration) {
 
         return duration.toSeconds() <= 0;
