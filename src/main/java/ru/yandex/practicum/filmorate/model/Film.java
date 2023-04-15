1070fa8ccc50d29156971219faf29379a93400ef
val2003 <val2003@yandex.ru>
2023-04-16 02:34:07 +0400

    reformat code 7

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
@@ -14,7 +13,7 @@ public class Film {
     private String name;
     private String description;
     private LocalDate releaseDate;
-    private Integer duration;
+    private int duration;
 
 
 }
